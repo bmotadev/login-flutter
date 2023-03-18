bfde72dea422575d158957fe43d724c181c76e18
Bruno Mota <bmotadev@gmail.com>
2023-03-18 17:02:20 -0300

    :recycle: refactor: alterado a cor da tela

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,125 +14,182 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 111, 219, 223),
-      body: SafeArea(
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Icon(
-              Icons.android,
-              size: 100,
-            ),
-            // Bem vindo
-            Text(
-              'Olá, novamente!',
-              style: GoogleFonts.bebasNeue(
-                fontSize: 52,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.deepPurple,
+            Colors.white,
+          ],
+        )),
+        child: SafeArea(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Icon(
+                    Icons.flutter_dash_sharp,
+                    size: 140,
+                    color: Colors.black,
+                    shadows: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.7),
+                        spreadRadius: 15,
+                        blurRadius: 17,
+                      )
+                    ],
+                  ),
+                  Icon(
+                    Icons.flutter_dash_sharp,
+                    size: 128,
+                    color: Colors.white,
+                  ),
+                ],
               ),
-            ),
-
-            Text(
-              'Como é bom ter você de volta!',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-
-            // campo de email
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 182, 243, 243),
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(12),
+              // Bem vindo
+              Text(
+                'Olá, tudo bem?',
+                style: GoogleFonts.fredokaOne(
+                  fontSize: 42,
+                  color: Colors.white,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Email',
+              ),
+
+              Text(
+                'Como é bom ter você de volta!',
+                style: GoogleFonts.fredoka(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+
+              // campo de email
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 241, 236, 255),
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 1.5,
+                          blurRadius: 2,
+                          offset: Offset(2, 4) // changes position of shadow
+                          ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Digite seu email',
+                        hintStyle: GoogleFonts.fredoka(),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
+              SizedBox(
+                height: 20,
+              ),
 
-            // campo de senha
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 182, 243, 243),
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Senha',
+              // campo de senha
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 241, 236, 255),
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+                          spreadRadius: 1.5,
+                          blurRadius: 2,
+                          offset: Offset(2, 4) // changes position of shadow
+                          ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Digite sua senha',
+                        hintStyle: GoogleFonts.fredoka(),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
+              const SizedBox(
+                height: 25,
+              ),
 
-            // botão de login
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    borderRadius: BorderRadius.circular(12)),
-                child: Center(
-                  child: Text(
-                    'Login',
+              // botão de login
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 1.5,
+                          blurRadius: 2,
+                          offset:
+                              const Offset(2, 4) // changes position of shadow
+                          ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Entrar',
+                      style: GoogleFonts.fredoka(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // Não tem conta? Registre-se
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Não tem conta? ',
                     style: TextStyle(
-                      color: Colors.white,
                       fontWeight: FontWeight.bold,
-                      fontSize: 18,
                     ),
                   ),
-                ),
+                  Text(
+                    'Registre-se',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-
-            // Não tem conta? Registre-se
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Não tem conta? ',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'Registre-se',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ]),
+            ]),
+          ),
         ),
       ),
     );
